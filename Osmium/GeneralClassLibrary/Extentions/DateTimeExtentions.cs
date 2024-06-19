2e4f6313841c131489c2159fc65e71a8f44ffb9c
Brenden Perry <brendenperry5@gmail.com>
2024-06-18 22:11:08 -0700

    Moved extentions

--- Osmium/GeneralClassLibrary/Extentions/DateTimeExtentions.cs
+++ Osmium/GeneralClassLibrary/Extentions/DateTimeExtentions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeneralClassLibrary.Utilities
+namespace GeneralClassLibrary.Extentions
 {
     public static class DateTimeExtentions
     {
